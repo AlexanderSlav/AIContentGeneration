218538c714d4e8216e2ff0c4923581bce029a2f3
AlexanderSlav <alexander.slavutin@gmail.com>
2023-06-15 15:25:31 +0300

    try to simplified

--- src/bot.py
+++ src/bot.py
@@ -12,89 +12,28 @@ from src.logger import get_logger
 logger = get_logger(__name__)
 
 
-class QuizForm(StatesGroup):
-    waiting_for_game_type = State()
-    waiting_for_movie_name = State()
-
-
 class QuizBot:
     def __init__(self):
         self.config = Config()
         self.bot = Bot(token=self.config.env_variables.telegram_bot_token)
-        storage = MemoryStorage()
-        self.dp = Dispatcher(self.bot, storage=storage)
+        self.dp = Dispatcher(self.bot)
 
-        self.handlers = {
-            ContentTypeRequest.guess_by_scene.value: GuessSceneRoundCreator(
-                self.bot, self.config
-            ),
-            ContentTypeRequest.guess_by_music_theme.value: GuessMusicRoundCreator(
-                self.bot, self.config
-            ),
-        }
+        self.handler = GuessSceneRoundCreator(self.bot, self.config)
 
-    async def handle_message(self, message: types.Message, state: FSMContext):
-        current_state = await state.get_state()
-        data = await state.get_data()
-        game_type = data.get(StateKeys.GAME_TYPE.value)
+    async def handle_message(self, message: types.Message):
+        logger.info("Handling message")
+        await self.handler.handle_message(message)
 
-        if current_state == QuizForm.waiting_for_movie_name.state and game_type:
-            handler = self.handlers[game_type]
-            await handler.handle_message(message)
-            await state.finish()  # reset the state when done
-        else:
-            await message.reply(
-                "I didn't understand that. Please choose an option from the menu."
-            )
-
-    async def handle_query(self, call: types.CallbackQuery, state: FSMContext):
-        data = await state.get_data()
-        logger.info(f"Callback data: {data}")
-        if game_type := data.get(StateKeys.GAME_TYPE.value):
-            handler = self.handlers[game_type]
-            await handler.handle_query(call)
-            await state.finish()  # reset the state when done
-        elif call.data in self.handlers:
-            await self.bot.send_message(
-                call.message.chat.id, "Send me a movie name. For example: The Matrix"
-            )
-            await QuizForm.waiting_for_game_type.set()
-            await state.update_data({StateKeys.GAME_TYPE.value: call.data})
-            await QuizForm.waiting_for_movie_name.set()  # add this line
-
-        elif call.data in ["confirm", "next"]:
-            logger.info(f"Received {call.data} from {call.from_user.id}")
-            # logger.info("Current state data:", call.data)
-            logger.info(f"Current game type {game_type}")
-            logger.info(
-                f"Current game type expected {data.get(StateKeys.GAME_TYPE.value)}"
-            )
-            if game_type := data.get(StateKeys.GAME_TYPE.value):
-                handler = self.handlers[game_type]
-                await handler.handle_query(call)
-                await state.finish()
-
-        else:
-            await self.bot.answer_callback_query(
-                call.id, "This feature is not implemented yet."
-            )
+    async def handle_query(self, call: types.CallbackQuery):
+        logger.info("Handling query")
+        await self.handler.handle_query(call)
 
     async def send_intro(self, message: types.Message):
-        markup = types.InlineKeyboardMarkup(row_width=1)
         movie_scene_btn = types.InlineKeyboardButton(
             "🎬 Get scene from movie",
             callback_data=ContentTypeRequest.guess_by_scene.value,
         )
-        movie_dialogue_btn = types.InlineKeyboardButton(
-            "💬 Get dialogue for movie",
-            callback_data=ContentTypeRequest.guess_by_dialogue.value,
-        )
-        movie_music_btn = types.InlineKeyboardButton(
-            "🎵 Get music theme for movie",
-            callback_data=ContentTypeRequest.guess_by_music_theme.value,
-        )
-        markup.add(movie_scene_btn, movie_dialogue_btn, movie_music_btn)
-        logger.info("Intro processed")
+        markup = types.InlineKeyboardMarkup().add(movie_scene_btn)
         await self.bot.send_message(
             message.chat.id, "Choose an option:", reply_markup=markup
         )
@@ -104,11 +43,8 @@ class QuizBot:
             self.send_intro, commands=["start", "hello", "intro"]
         )
         self.dp.register_message_handler(
-            self.handle_message,
-            state=[QuizForm.waiting_for_movie_name, QuizForm.waiting_for_game_type],
-            content_types=types.ContentTypes.TEXT,
+            self.handle_message, content_types=types.ContentTypes.TEXT
         )
-        self.dp.register_callback_query_handler(self.handle_query, state="*")
-
+        self.dp.register_callback_query_handler(self.handle_query)
         logger.info("Bot is running")
         await self.dp.start_polling()
--- src/guess_by_frame/creator.py
+++ src/guess_by_frame/creator.py
@@ -14,33 +14,23 @@ logger = get_logger(__name__)
 class GuessSceneRoundCreator:
     def __init__(self, bot, config: Config):
         self.bot = bot
-        self.api_key: str = config.env_variables.google_api_key
-        self.cx: str = config.env_variables.google_cx
-        self.output_video_path: str = config.out_video_params.output_path
-        self.download_path: str = config.guess_scene_params.download_path
-        self.num_scenes_per_round: str = config.guess_scene_params.num_scenes_per_round
-        self.num_scenes_to_search: str = config.guess_scene_params.num_scenes_to_search
-        self.movie_search_engine: GoogleAPISceneEngine = GoogleAPISceneEngine(
-            self.num_scenes_to_search, self.api_key, self.cx
+        self.movie_search_engine = GoogleAPISceneEngine(
+            config.guess_scene_params.num_scenes_to_search,
+            config.env_variables.google_api_key,
+            config.env_variables.google_cx,
         )
         self.video_generator = VideoGenerator(config)
-        self.scene_collector: SceneCollector = SceneCollector()
-        self.image_counter: int = 0
+        self.scene_collector = SceneCollector()
 
     async def handle_message(self, message):
-        self.movie_name = message.text
-        self.image_counter = 0
-        self.images = await self.movie_search_engine.search(self.movie_name)
+        movie_name = message.text
+        images = await self.movie_search_engine.search(movie_name)
 
-        if not self.images or len(self.images) == 0:
-            await self.bot.send_message(message.chat.id, "No more images.")
+        if not images:
+            await self.bot.send_message(message.chat.id, "No images found.")
             return
 
-        for image in self.images:
-            if self.image_counter >= self.movie_search_engine.num_frames_per_request:
-                break
-
-            logger.debug(f"Sending image {self.image_counter}")
+        for image in images:
             markup = types.InlineKeyboardMarkup()
             confirm_btn = types.InlineKeyboardButton("✅", callback_data="confirm")
             next_btn = types.InlineKeyboardButton("❌", callback_data="next")
@@ -48,33 +38,29 @@ class GuessSceneRoundCreator:
 
             # send the image without downloading
             await self.bot.send_photo(message.chat.id, image.url, reply_markup=markup)
-            self.image_counter += 1
 
     async def handle_query(self, call):
-        logger.info(f"Callback query: {call}")
         if call.data == "confirm":
             # download the image when it is confirmed
             image = self.images.pop(0)
-            await self.movie_search_engine.download([image], self.download_path)
+            await self.movie_search_engine.download([image])
 
-            logger.info("DEBUG", call.message.photo[-1].file_id, call.message.caption)
-            print(call.message.photo[-1].file_id, call.message.caption)
-            scene = Scene(call.message.photo[-1].file_id, call.message.caption)
+            scene = Scene(call.message.photo[-1].file_id)
             self.scene_collector.add_scene(scene)
             await self.bot.answer_callback_query(call.id, "Image confirmed.")
+
+            # check if we have enough scenes and generate the video
+            if (
+                len(self.scene_collector.get_scenes())
+                >= self.movie_search_engine.num_frames_per_request
+            ):
+                await self.generate_and_send_video(call.message.chat.id)
+
         elif call.data == "next":
             await self.bot.answer_callback_query(call.id, "Next image.")
-        if (
-            self.movie_search_engine
-            and self.image_counter < self.movie_search_engine.num_frames_per_request
-        ):
-            await self.handle_message(call.message)
 
-    def parse_scenes(self, scenes: List[Scene]) -> List[str]:
-        return [scene.image_path for scene in scenes]
-
-    async def generate_and_send_video(self, chat_id: int, music_path: str):
-        image_paths = self.parse_scenes(self.scene_collector.get_scenes())
+    async def generate_and_send_video(self, chat_id: int):
+        image_paths = [scene.image_path for scene in self.scene_collector.get_scenes()]
         self.video_generator.generate_video(image_paths=image_paths)
 
         with open("output.mp4", "rb") as video:

9d1e96feae5b0925c40a91b4fb05732a05f5f098
AlexanderSlav <alexander.slavutin@gmail.com>
2023-06-19 01:28:06 +0300

    fix errors

--- tests/test_scene_collector.py
+++ tests/test_scene_collector.py
@@ -1,5 +1,11 @@
+from src.guess_by_frame.utils import Scene
+
+
+mock_scene = Scene(path_to_image="path_to_image_1", movie_name="test_movie")
+mock_scene_2 = Scene(path_to_image="path_to_image_2", movie_name="test_movie_2")
+
 def test_scene_collector(scene_collector):
     # Test add_image and get_images
-    scene_collector.add_image("path_to_image1")
-    scene_collector.add_image("path_to_image2")
-    assert scene_collector.get_images() == ["path_to_image1", "path_to_image2"]
+    scene_collector.add_scene(mock_scene)
+    scene_collector.add_scene(mock_scene_2)
+    assert scene_collector.get_scenes() == [mock_scene, mock_scene_2]
